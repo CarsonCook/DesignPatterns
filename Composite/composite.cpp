6d4098c635ddbe21bcf29b57dc4499ccd2e321b5
Carson Cook <cook.carson38@gmail.com>
2017-05-19 18:09:50 -0400

    made code for composite

--- Composite/composite.cpp
+++ Composite/composite.cpp
@@ -1,10 +1,12 @@
 #include "composite.hh"
 #include <string>
 #include <iostream>
+#include <vector>
 
-Employee::Employee(std::string name)
+Employee::Employee(std::string name, int salary)
 {
     mName=name;
+    mSalary=salary;
 }
 
 void Employee::setName(std::string name)
@@ -15,4 +17,61 @@ void Employee::setName(std::string name)
 std::string Employee::getName()
 {
     return mName;
+}
+
+void Employee::setSalary(int salary)
+{
+    mSalary=salary;
+}
+
+int Employee::getSalary()
+{
+    return mSalary;
+}
+
+Designer::Designer(std::string name, int salary):Employee(name,salary){};
+
+std::string Designer::getRole()
+{
+    return "Designer";
+}
+
+void Designer::printEmployee()
+{
+    std::cout << getName() << " " << getRole() << " " << getSalary() << std::endl;
+}
+
+Developer::Developer(std::string name, int salary):Employee(name,salary){};
+
+std::string Developer::getRole()
+{
+    return "Developer";
+}
+
+void Developer::printEmployee()
+{
+    std::cout << getName() << " " << getRole() << " " << getSalary() << std::endl;
+}
+
+Business::~Business()
+{
+    for (int i=0; i<mEmployees.size(); i++)
+    {
+        delete mEmployees[i];
+    }
+}
+
+void Business::addEmployee(Employee* employee)
+{
+    mEmployees.push_back(employee);
+}
+
+int Business::getNetSalaries()
+{
+    int salarySum=0;
+    for (int i=0; i<mEmployees.size(); i++)
+    {
+        salarySum+=mEmployees[i]->getSalary();
+    }
+    return salarySum;
 }
--- Composite/composite.hh
+++ Composite/composite.hh
@@ -2,18 +2,48 @@
 #define composite_hh
 
 #include <string>
+#include <vector>
 
 class Employee
 {
 public:
-    Employee(std::string name);
-    virtual int getSalary()=0;
+    Employee(std::string name, int salary);
+    virtual ~Employee(){};
+    int getSalary();
+    void setSalary(int);
     std::string getName();
     void setName(std::string);
     virtual std::string getRole()=0;
     virtual void printEmployee()=0;
 private:
     std::string mName;
+    int mSalary;
+};
+
+class Designer : public Employee
+{
+public:
+    Designer(std::string name, int salary);
+    std::string getRole();
+    void printEmployee();
+};
+
+class Developer : public Employee
+{
+public:
+    Developer(std::string name, int salary);
+    std::string getRole();
+    void printEmployee();
+};
+
+class Business
+{
+public:
+    ~Business();
+    void addEmployee(Employee* employee);
+    int getNetSalaries();
+private:
+    std::vector<Employee*> mEmployees;
 };
 
 #endif // composite_hh
