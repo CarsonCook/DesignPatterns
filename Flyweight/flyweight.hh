159955b443a21412147bdf65a9de7cef2c96d4eb
Carson Cook <cook.carson38@gmail.com>
2017-05-21 12:10:40 -0400

    commit because need to ref online for flyweight and cant rightnow - going to other pattern for now

--- Flyweight/flyweight.hh
+++ Flyweight/flyweight.hh
@@ -1,6 +1,24 @@
 #ifndef flyweight_hh
 #define flyweight_hh
 
+#include <vector>
+#include <string>
 
+class ChaiTea
+{
+
+};
+class MintTeea
+{
+
+};
+
+class TeaMaker
+{
+public:
+
+private:
+    std::vector<std::string> mTeas;
+};
 
 #endif
